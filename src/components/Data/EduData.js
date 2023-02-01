49777ef180d2a62fdbbb601c19e7b1f4fd6e3cc2
MrPolloo <nauvalsm@gmail.com>
2023-02-01 08:58:09 +0700

    Add ID

--- src/components/Data/EduData.js
+++ src/components/Data/EduData.js
@@ -1,17 +1,20 @@
 export const EduData = [
     {
+        id: '1',
         tempat: 'Universitas Lampung',
         tingkatan: 'S1',
         jurusan: 'Teknik Informatika',
         tahun: '2021-2024'
     },
     {
+        id: '2',
         tempat: 'Universitas Lampung',
         tingkatan: 'S1',
         jurusan: 'Teknik Informatika',
         tahun: '2021-2024'
     },
     {
+        id: '3',
         tempat: 'Universitas Lampung',
         tingkatan: 'S1',
         jurusan: 'Teknik Informatika',
--- src/components/Data/ExpData.js
+++ src/components/Data/ExpData.js
@@ -1,17 +1,20 @@
 export const ExpData = [
     {
+        id: '1',
         judul: 'Judul Experience',
         tempat: 'Catalina',
         jabatan: 'RT/RW',
         tahun: '2021-2024'
     },
     {
+        id: '2',
         judul: 'Judul Experience',
         tempat: 'Catalina',
         jabatan: 'RT/RW',
         tahun: '2021-2024'
     },
     {
+        id: '3',
         judul: 'Judul Experience',
         tempat: 'Catalina',
         jabatan: 'RT/RW',
